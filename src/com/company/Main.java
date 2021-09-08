71994c8a1be260ddd1518acc76675d713a9b2bfb
rico45n <rpesnin@mail.ru>
2021-09-09 00:00:25 +0300

    Init

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,45 +5,67 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner osnv = new Scanner(System.in);
-        double firstPer;
-        double secondPer;
-        char simvoli;
-        double result;
-        System.out.println("Here is a simple calculator ");
 
-        System.out.println("Enter the first value ");
-        firstPer = osnv.nextDouble();
+        Scanner a = new Scanner(System.in);
 
-        System.out.println("Select an action (+)(-)(*)(/) ");
-        simvoli = osnv.next().charAt(0);
+        String com1;
+        String sos1;
 
-        System.out.println("Enter the second value ");
-        secondPer = osnv.nextDouble();
+        System.out.println("Enter the first command:");
+        com1=a.nextLine();
 
-        switch (simvoli){
-            case '+':
-                result = firstPer + secondPer;
-                System.out.println("Result:" + result);
-                break;
+        System.out.println("Enter the composition separated by commas :");
+        sos1=a.nextLine();
 
-            case '-':
-                result = firstPer - secondPer;
-                System.out.println("Result:" + result);
-                break;
 
-            case '*':
-                result = firstPer * secondPer;
-                System.out.println("Result:" + result);
-                break;
+        String rezylt1 = com1 + ": "+ sos1;
+        System.out.println(rezylt1);
+        System.out.println("The first team is ready");
 
-            case '/':
-                result=firstPer/secondPer;
-                System.out.println("Result:" + result);
-                break;
 
-            default:
-                System.out.println("Error");
-        }
+
+
+
+//        //Col
+//        Scanner osnv = new Scanner(System.in);
+//        double firstPer;
+//        double secondPer;
+//        char simvoli;
+//        double result;
+//        System.out.println("Here is a simple calculator ");
+//
+//        System.out.println("Enter the first value ");
+//        firstPer = osnv.nextDouble();
+//
+//        System.out.println("Select an action (+)(-)(*)(/) ");
+//        simvoli = osnv.next().charAt(0);
+//
+//        System.out.println("Enter the second value ");
+//        secondPer = osnv.nextDouble();
+//
+//        switch (simvoli){
+//            case '+':
+//                result = firstPer + secondPer;
+//                System.out.println("Result:" + result);
+//                break;
+//
+//            case '-':
+//                result = firstPer - secondPer;
+//                System.out.println("Result:" + result);
+//                break;
+//
+//            case '*':
+//                result = firstPer * secondPer;
+//                System.out.println("Result:" + result);
+//                break;
+//
+//            case '/':
+//                result=firstPer/secondPer;
+//                System.out.println("Result:" + result);
+//                break;
+//
+//            default:
+//                System.out.println("Error");
+//        }
     }
 }
