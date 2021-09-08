f6eb738b8fefa2597d9ebc35bd46e1ccfbabf95c
rico45n <rpesnin@mail.ru>
2021-09-09 00:07:58 +0300

    Reboot

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,66 +6,73 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner a = new Scanner(System.in);
 
-        String com1;
-        String sos1;
+        Scanner osnv = new Scanner(System.in);
+        double firstPer;
+        double secondPer;
+        char simvoli;
+        double result;
+        System.out.println("Here is a simple calculator ");
 
-        System.out.println("Enter the first command:");
-        com1=a.nextLine();
+        System.out.println("Enter the first value ");
+        firstPer = osnv.nextDouble();
 
-        System.out.println("Enter the composition separated by commas :");
-        sos1=a.nextLine();
+        System.out.println("Select an action (+)(-)(*)(/) ");
+        simvoli = osnv.next().charAt(0);
+
+        System.out.println("Enter the second value ");
+        secondPer = osnv.nextDouble();
+
+        switch (simvoli){
+            case '+':
+                result = firstPer + secondPer;
+                System.out.println("Result:" + result);
+                break;
+
+            case '-':
+                result = firstPer - secondPer;
+                System.out.println("Result:" + result);
+                break;
+
+            case '*':
+                result = firstPer * secondPer;
+                System.out.println("Result:" + result);
+                break;
+
+            case '/':
+                result=firstPer/secondPer;
+                System.out.println("Result:" + result);
+                break;
+
+            default:
+                System.out.println("Error");
+        }
 
 
-        String rezylt1 = com1 + ": "+ sos1;
-        System.out.println(rezylt1);
-        System.out.println("The first team is ready");
 
 
 
 
-
-//        //Col
-//        Scanner osnv = new Scanner(System.in);
-//        double firstPer;
-//        double secondPer;
-//        char simvoli;
-//        double result;
-//        System.out.println("Here is a simple calculator ");
+        //        Scanner a = new Scanner(System.in);
 //
-//        System.out.println("Enter the first value ");
-//        firstPer = osnv.nextDouble();
+//        String com1;
+//        String sos1;
 //
-//        System.out.println("Select an action (+)(-)(*)(/) ");
-//        simvoli = osnv.next().charAt(0);
+//        System.out.println("Enter the first command:");
+//        com1=a.nextLine();
 //
-//        System.out.println("Enter the second value ");
-//        secondPer = osnv.nextDouble();
+//        System.out.println("Enter the composition separated by commas :");
+//        sos1=a.nextLine();
 //
-//        switch (simvoli){
-//            case '+':
-//                result = firstPer + secondPer;
-//                System.out.println("Result:" + result);
-//                break;
 //
-//            case '-':
-//                result = firstPer - secondPer;
-//                System.out.println("Result:" + result);
-//                break;
-//
-//            case '*':
-//                result = firstPer * secondPer;
-//                System.out.println("Result:" + result);
-//                break;
-//
-//            case '/':
-//                result=firstPer/secondPer;
-//                System.out.println("Result:" + result);
-//                break;
-//
-//            default:
-//                System.out.println("Error");
-//        }
+//        String rezylt1 = com1 + ": "+ sos1;
+//        System.out.println(rezylt1);
+//        System.out.println("The first team is ready");
+
+
+
+
+
+
     }
 }
